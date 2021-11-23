188dcb7ff0f50ad0836f6974b80a80b7b600db6e
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-11-23 18:46:26 -0500

    local MAIN: create containers

---
+++ src/containers/ProductDetail.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ProductInfo from '../components/ProductInfo';
+import '../styles/ProductDetail.scss';
+
+import close from '@icons/icon_close.png';
+
+const ProductDetail = ({ product, setToggleProduct, handleClick }) => {
+	return (
+		<aside className="ProductDetail">
+			<div className="ProductDetail-close" onClick={() => setToggleProduct(false)}>
+				<img src={close} alt="close" />
+			</div>
+			<ProductInfo
+				product={product}
+				setToggleProduct={setToggleProduct}
+				handleClick={handleClick}
+			/>
+		</aside>
+	);
+}
+
+export default ProductDetail;
